25927c197e4e84eafb19bab7a396d6cccd119ecf
Joao Vicente <joao@upsites.digital>
2021-11-25 22:56:44 -0300

    Melhorando a verificaçao e exibiçao da mensagem de erro

--- js/main.js
+++ js/main.js
@@ -17,16 +17,18 @@ var alturaEhValida = true;
 if(peso <= 0 || peso >= 1000) {
     console.log("Peso inválido!");
     pesoEhValido = false;
-    tdImc.textContent = "Peso inválido!";
+    tdPeso.textContent = "Peso inválido!";
 }
 
 if(altura <= 0 || altura >= 3.00) {
     console.log("Altura inválida!");
     alturaEhValida = false;
-    tdImc.textContent = "Altura inválida!";
+    tdAltura.textContent = "Altura inválida!";
 }
 
 if(alturaEhValida && pesoEhValido) {
     var imc = peso / (altura * altura);
     tdImc.textContent = imc;
+} else {
+    tdImc.textContent = "Altura e/ou peso inválidos!"
 }
