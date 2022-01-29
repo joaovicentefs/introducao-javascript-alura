20f0ba4c2c68ff21a1036c274bedb3c1e716f222
Joao Vicente <joao@upsites.digital>
2022-01-29 14:49:01 -0300

    Aprendendo a inserir uma mensagem de erro na validcao do formulario

--- js/form.js
+++ js/form.js
@@ -8,8 +8,10 @@ botaoAdicionar.addEventListener("click",function (event){
 //Aninha as Tds dentro da Tr
     var pacienteTr = montaTr(paciente);
 
-    if(!validaPaciente(paciente)){
-        console.log("Paciente inválido");
+    var erro = validaPaciente(paciente);
+    if(erro.length > 0){
+        var mensagemErro = document.querySelector('#mensagem-erro');
+        mensagemErro.textContent = erro;
         return;
     }
 //adiciona o paciente na tabela
@@ -50,9 +52,9 @@ function montaTd(dado, classe){
 
 function validaPaciente(paciente){
     if(validaPeso(paciente.peso)){
-        return true;
+        return "";
     }
     else {
-        return false;
+        return "O peso é inválido!";
     }
 }
